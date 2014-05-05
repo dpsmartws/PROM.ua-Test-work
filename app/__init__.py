ad1853ec9b469effeed5e6f5a764e9b172bb6248
dpsmartws <dp.smartws@gmail.com>
2014-05-05 05:10:00 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,20 +7,6 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 
-class CustomJSONEncoder():
-    def default(self, obj):
-        try:
-            if isinstance(obj, date):
-                return obj.isoformat()
-            iterable = iter(obj)
-        except TypeError:
-            pass
-        else:
-            return list(iterable)
-        return JSONEncoder.default(self, obj)
-
-app.json_encoder = CustomJSONEncoder
-
 from view import *
 from models.users import *
 from models.library import *
